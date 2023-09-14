63ca77e7069a23f0c9faef62c1c22a51fb35959d
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-09-14 11:56:53 -0300

    criação interfaces

---
+++ StockShoe/StockShoe/Interfaces/IStockReader.cs
@@ -0,0 +1,15 @@
+﻿using StockShoe.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockShoe.Interfaces
+{
+    public interface IStockReader
+    {
+        List<Shoe> GetAllShoes();
+        Shoe GetShoeById(int shoeId);
+    }
+}
---
+++ StockShoe/StockShoe/Interfaces/IStockWriter.cs
@@ -0,0 +1,16 @@
+﻿using StockShoe.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockShoe.Interfaces
+{
+    public interface IStockWriter
+    {
+        void AddShoe(Shoe shoe);
+        void UpdateShoe(Shoe shoe);
+        void RemoveShoe(int shoeId);
+    }
+}
